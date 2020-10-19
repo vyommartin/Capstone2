aa4db1c37450aac8525d15bb626fdc0d4cb86fd3
Vyom Martin <63099980+vyommartin@users.noreply.github.com>
2020-10-19 22:40:35 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -1,23 +1,6 @@
-var el = x => document.getElementById(x);
-
-function showPicker() {
-  el("file-input").click();
-}
-
-function showPicked(input) {
-  el("upload-label").innerHTML = input.files[0].name;
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    el("image-picked").src = e.target.result;
-    el("image-picked").className = "";
-  };
-  reader.readAsDataURL(input.files[0]);
-}
-
 function analyze() {
-  var uploadFiles = el("file-input").files;
-  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
-
+  var uploadFiles = document.getElementById("file-input").value;
+  
   el("analyze-button").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
