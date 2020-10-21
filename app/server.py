0bfd52a3e373714f9658a11e1901e5e9a5205057
Vyom Martin <63099980+vyommartin@users.noreply.github.com>
2020-10-21 17:33:46 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,52 +10,18 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 from fast_bert.data_cls import BertDataBunch
 from fast_bert.learner_cls import *
+from fast_bert.prediction import *
+import torch
 import sys
 
 export_file_url = "https://www.googleapis.com/drive/v3/files/1KjrRxptv78tXKGYCE-B6EsRJ2e8YkUDm?alt=media&key=AIzaSyCNoRufM9Z-HSsX566HZv-Qj2shWUI7BBs"
 export_file_name = 'pytorch_model.bin'
 
-path = Path('/root/Capstone2/app/')
+path = Path('/root/Capstone2/app')
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='/root/Capstone2/app/static'))
-
-async def setup_learner():
-    try:
-        data_bunch = BertDataBunch(path, path,
-                                   tokenizer = path,
-                                   train_file = None,
-                                   val_file = None,
-                                   label_file = 'l2.csv',
-                                   batch_size_per_gpu = 120,
-                                   max_seq_length = 40,
-                                   multi_gpu = False,
-                                   multi_label = False,
-                                   model_type = None)
-        model = load_model(data_bunch, path, path / export_file_name, device = "cpu", multi_label = False)
-        learn = BertLearner(data_bunch, model, path, metrics = [], output_dir = None, device = 'cpu', logger = None)
-        #learn = BertLearner.from_pretrained_model(data_bunch, 
-                                            #pretrained_path = path,
-                                            #metrics = [],
-                                            #device = 'cpu',
-                                            #logger = None,
-                                            #output_dir = None,
-                                            #is_fp16 = False)
-        return learn
-    except RuntimeError as e:
-        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
-            print(e)
-            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
-            raise RuntimeError(message)
-        else:
-            raise
-
-
-loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future(setup_learner())]
-learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
-loop.close()
 
 
 @app.route('/')
@@ -66,18 +32,24 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
- text = await request.form()
- preds = learner.predict_batch([text])
- return JSONResponse({'result': str(preds), 'probability': str(probability)})
-       
- #  img_data = await request.form()
-     
-#    img_bytes = await (img_data['file'].read())
- #   img = open_image(BytesIO(img_bytes))
-  #  prediction, pred_idx, probs = learn.predict(img)
-  #  probability = str(round(float((max(probs)*100).numpy()), 2)) + "%"
-  
- 
+
+    data_bunch = await BertDataBunch(path, path,
+                                     tokenizer = path,
+                                     train_file = None,
+                                     val_file = None,
+                                     label_file = 'l2.csv',
+                                     batch_size_per_gpu = 120,
+                                     max_seq_length = 40,
+                                     multi_gpu = False,
+                                     multi_label = False,
+                                     model_type = None)
+
+    model = await load_model(data_bunch, path, path / export_file_name, device = "cpu", multi_label = False)
+    learner = await BertLearner(data_bunch, model, path, metrics = [], output_dir = None, device = 'cpu', logger = None)
+
+    text = await request.form()
+    preds = learner.predict_batch([text])
+    return JSONResponse({'result': str(preds), 'probability': str(probability)})
 
 
 if __name__ == '__main__':
